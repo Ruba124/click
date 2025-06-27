4b2325aa5da54d4869c79c88066169b63fb86911
Ruba124 <139155246+Ruba124@users.noreply.github.com>
2025-06-28 01:15:16 +0300

    Add files via upload

---
+++ ja.js
@@ -0,0 +1,11 @@
+
+let A1 = document.getElementById("jo");
+let A2 = document.getElementById("la");
+A1.addEventListener('click',fine);
+function fine()
+{
+
+   A2.textContent=' you are human';
+
+
+}
